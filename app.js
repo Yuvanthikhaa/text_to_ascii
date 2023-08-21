a3cd43fdb1a2ddaf35f4eae6079128b209083a26
Yuvanthikhaa <athivelyuvi@gmail.com>
2023-08-21 13:02:09 +0530

    inital commit

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-var student={
-    name:"",
-    type:"student"
-};
-document.addEventListener("DOMContentLoaded",contentloaded);
- function contentloaded(event)
- {
-    document.getElementById('name').addEventListener("keyup",keyUp)
- }
- function keyUp(event){
-    calculateNumericOutput();
- }
- function calculateNumericOutput(){
-    student.name=document.getElementById('name').value;
-    var totalNameValue=0;
-    for(var i=0;i<student.name.length;i++){
-        totalNameValue+=student.name.charCodeAt(i);
-    }
-    var output="total numeric value is: "+totalNameValue;
-    document.getElementById('output').innerText=output;
- }
+(function(){
+   'use strict';
+   angular.module('myFirstAngularApp',[])
+   .controller('MyFirstController', function($scope){
+      $scope.name="max";
+   });
+   
+})();
